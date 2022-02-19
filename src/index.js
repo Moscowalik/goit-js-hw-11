f440214d346690398ae412205c75530c3a10dcdb
Moscowalik <moscowalik95@gmail.com>
2022-02-19 21:43:28 +0200

    change mistake

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/style.css';
-import ApiSettings from './APISettings';
+import ApiSettings from './APISettings.js';
 import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
